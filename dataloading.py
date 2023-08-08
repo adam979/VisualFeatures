45ffeff3c74683a08f85e75d05b596ee4dcd1bae
SufiyanAdam <88035@arsd.du.ac.in>
2023-08-08 22:59:30 +0530

    Feature update in the SwinV2 file

--- dataloading.py
+++ dataloading.py
@@ -14,15 +14,6 @@ train_dir = os.path.join(root_dir, "train")
 val_dir = os.path.join(root_dir, "val")
 test_dir = os.path.join(root_dir, "test")
 
-
-# # Apply adaptive histogram equalization
-# def apply_equalize_adapthist(x):
-#     if isinstance(x, np.ndarray):
-#         x = torch.from_numpy(x)  # Convert the image to a Tensor
-#     x = exposure.equalize_adapthist(x.numpy(), clip_limit=0.03)
-#     return x
-
-
 # Define the transformation applied to each image
 transform = transforms.Compose(
     [
@@ -43,7 +34,7 @@ val_dataset = datasets.ImageFolder(val_dir, transform=transform)
 test_dataset = datasets.ImageFolder(test_dir, transform=transform)
 
 # Create data loaders to iterate over the datasets
-batch_size = 32
+batch_size = 8
 num_workers = 4
 
 train_loader = torch.utils.data.DataLoader(
@@ -61,31 +52,49 @@ test_loader = torch.utils.data.DataLoader(
 def main():
     # Initialize the Swin Transformer model
     model = SwinV2()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
     # Extract visual features using the Swin Transformer model
+    # Modify the extract_swin_features function
     def extract_swin_features(loader, model):
-        features = []
+        global_features_list = []
+        local_features_list = []
         labels = []
-        for images, targets in tqdm(loader, desc="Extacting Features"):
-            visual_features = extract_visual_features(images, model)
-            features.append(visual_features)
+        print("Starting feature extraction...")
+        for i, (images, targets) in enumerate(loader):
+            images = images.to(device)  # Move images to the GPU if available
+            global_features, local_features = extract_visual_features(images, model)
+            global_features_list.append(global_features)
+            local_features_list.append(local_features)
             labels.append(targets)
-        features = torch.cat(features)
-        labels = torch.cat(labels)
-        return features, labels
+            print(f"Iteration {i+1}/{len(loader)} completed.")
+        global_features = torch.cat(global_features_list, dim=0)
+        local_features = torch.cat(local_features_list, dim=0)
+        labels = torch.cat(labels, dim=0)
+        print("Feature extraction completed.")
+        return global_features, local_features, labels
 
-    # Extract visual features from the train, validation, and test datasets
-    train_features, train_labels = extract_swin_features(train_loader, model)
-    val_features, val_labels = extract_swin_features(val_loader, model)
-    test_features, test_labels = extract_swin_features(test_loader, model)
+    # Extract global and local features from the train, validation, and test datasets
+    train_global_features, train_local_features, train_labels = extract_swin_features(
+        train_loader, model
+    )
+    val_global_features, val_local_features, val_labels = extract_swin_features(
+        val_loader, model
+    )
+    test_global_features, test_local_features, test_labels = extract_swin_features(
+        test_loader, model
+    )
+
+    # Merge the global and local features
+    train_features = torch.cat((train_global_features, train_local_features), dim=1)
+    val_features = torch.cat((val_global_features, val_local_features), dim=1)
+    test_features = torch.cat((test_global_features, test_local_features), dim=1)
 
     # Save the extracted features and labels for future use
     torch.save(train_features, "train_features.pt")
-    torch.save(train_labels, "train_labels.pt")
     torch.save(val_features, "val_features.pt")
-    torch.save(val_labels, "val_labels.pt")
     torch.save(test_features, "test_features.pt")
-    torch.save(test_labels, "test_labels.pt")
 
 
 if __name__ == "__main__":
--- swinv2.py
+++ swinv2.py
@@ -1,6 +1,18 @@
 import torch
+import torch.nn.functional as F
 import torch.nn as nn
 from torchvision.models import swin_transformer
+
+
+class LocalFeatureExtractor(nn.Module):
+    def __init__(self, in_channels):
+        super(LocalFeatureExtractor, self).__init__()
+
+        self.conv = nn.Conv2d(in_channels, 10, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        return self.relu(self.conv(x))
 
 
 class SwinV2(nn.Module):
@@ -28,11 +40,38 @@ class SwinV2(nn.Module):
         # Remove the classification head
         self.backbone.head = nn.Identity()
 
+        # Add additional layers to extract local features
+        self.local_features = LocalFeatureExtractor(96)
+
     def forward(self, x):
-        x = self.backbone(x)
-        return x
+        global_features = self.backbone(x)
+        local_features = self.local_features(global_features)
+        return global_features, local_features
 
 
 def extract_visual_features(images, model):
-    features = model(images)
-    return features
+    # Convert grayscale images to 3 channels (assuming input is grayscale with 1 channel)
+    if images.size(1) == 1:
+        images_rgb = images.repeat(1, 3, 1, 1)
+    else:
+        images_rgb = images
+
+    print("Shape of images_rgb:", images_rgb.size())
+
+    # Extract features using the modified SwinV2 model
+    global_features, local_features = model(images_rgb)
+    print("Shape of global_features (after Swin Transformer):", global_features.size())
+    print("Shape of local_features (before convolution):", local_features.size())
+
+    # Apply adaptive average pooling to global_features to resize it to a fixed spatial size
+    global_features = F.adaptive_avg_pool2d(global_features, (7, 7))
+
+    # Apply the convolution operation to the local features tensor
+    local_features = model.local_features(global_features)
+
+    print(
+        "Shape of global_features (after adaptive avg pooling):", global_features.size()
+    )
+    print("Shape of local_features (after convolution):", local_features.size())
+
+    return global_features, local_features
